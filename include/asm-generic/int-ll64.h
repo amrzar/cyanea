c399b096d53f0a89f4aa973a745fef9bea72f6ec
Amirreza Zarrabi <amrzar@gmail.com>
2020-07-07 14:31:54 +1000

    include int-ll64.h
    add definition of s/u8, a/u16, s/u32, and s/u64.
    add definition of S/U8_C, S/U16_C, S/U32_C, and S/U64_C macros.

---
+++ include/asm-generic/int-ll64.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 Amirreza Zarrabi <amrzar@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __ASM_GENERIC_INT_LL64_H
+#define __ASM_GENERIC_INT_LL64_H
+
+#include <asm/bitsperlong.h>
+
+#ifndef __ASSEMBLY__
+
+typedef signed char s8;
+typedef unsigned char u8;
+ 
+typedef signed short s16;
+typedef unsigned short u16;
+ 
+typedef signed int s32;
+typedef unsigned int u32;
+ 
+typedef signed long long s64;
+typedef unsigned long long u64;
+
+#define S8_C(x)  x
+#define U8_C(x)  x ## U
+#define S16_C(x) x
+#define U16_C(x) x ## U
+#define S32_C(x) x
+#define U32_C(x) x ## U
+#define S64_C(x) x ## LL
+#define U64_C(x) x ## ULL
+
+#else /* __ASSEMBLY__ */
+
+#define S8_C(x)  x
+#define U8_C(x)  x
+#define S16_C(x) x
+#define U16_C(x) x
+#define S32_C(x) x
+#define U32_C(x) x
+#define S64_C(x) x
+#define U64_C(x) x
+
+#endif /* __ASSEMBLY__ */
+#endif /* __ASM_GENERIC_INT_LL64_H */
