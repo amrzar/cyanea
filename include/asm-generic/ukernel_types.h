c5ef49e6c4f1dc873b7b25549bebc13eace2bb23
Amirreza Zarrabi <amrzar@gmail.com>
2020-07-08 12:28:23 +1000

    include ukernek_types.h
    use this file for kernel dependant types such as capability descriptors.

---
+++ include/asm-generic/ukernel_types.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2020 Amirreza Zarrabi <amrzar@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __ASM_GENERIC_UKERNEL_TYPES_H
+#define __ASM_GENERIC_UKERNEL_TYPES_H
+
+typedef unsigned long __ukernel_size_t;
+typedef long __ukernel_ssize_t;
+typedef long __ukernel_ptrdiff_t;
+
+#endif /* __ASM_GENERIC_UKERNEL_TYPES_H */
