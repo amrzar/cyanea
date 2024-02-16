6ea2e49ae55b0648b06c143d6d1b739efbed40a4
Amirreza Zarrabi <amrzar@gmail.com>
2024-02-17 10:40:30 +1100

    acpi: add 'acpi_table_init'
    Signed-off-by: Amirreza Zarrabi <amrzar@gmail.com>

--- drivers/acpi/acpi.c
+++ drivers/acpi/acpi.c
@@ -1,1 +1,38 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <cyanea/acpi.h>
+#include <cyanea/errno.h>
+
+# define ACPI_MAX_INIT_TABLES 128
+static struct acpi_table_desc initial_tables[ACPI_MAX_INIT_TABLES];
+
+static int __init acpi_locate_initial_tables(void)
+{
+    ACPI_STATUS status;
+
+    /* Initialize the ACPICA table manager and get ACPI tables. */
+
+    status = AcpiInitializeTables(initial_tables, ACPI_MAX_INIT_TABLES, FALSE);
+    if (ACPI_FAILURE(status))
+        return -EINVAL;
+
+    return SUCCESS;
+}
+
+static void __init acpi_table_init_complete(void)
+{
+    /* TODO. Support ACPI table override. */
+}
+
+int __init acpi_table_init(void)
+{
+    int err;
+
+    err = acpi_locate_initial_tables();
+    if (err)
+        return err;
+
+    acpi_table_init_complete();
+
+    return 0;
+}
--- include/cyanea/acpi.h
+++ include/cyanea/acpi.h
@@ -3,4 +3,11 @@
 #ifndef __CYANEA_ACPI_H__
 #define __CYANEA_ACPI_H__
 
+#include <cyanea/init.h>
+
+# undef __linux__
+# include "../../drivers/acpi/acpica/source/include/acpi.h"
+
+int __init acpi_table_init(void);
+
 #endif /* __CYANEA_ACPI_H__ */
