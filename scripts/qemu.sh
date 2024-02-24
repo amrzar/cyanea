20983d12f4876a1a90377e5a67659a70c2c4a708
Amirreza Zarrabi <amrzar@gmail.com>
2024-02-25 09:51:06 +1100

    scripts: add NUMA support for QEMU
    Signed-off-by: Amirreza Zarrabi <amrzar@gmail.com>

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -31,6 +31,37 @@ case ${ARCH} in
                 -nographic \
                 -append "console=uart,io,0x3f8,9600 memblock=debug" \
                 "$@" "${DEBUG_OPTIONS[@]}" &> /dev/null &
+        elif [[ -v NUMA ]]; then
+            #
+            # ┌────────┐    ┌────────┐
+            # | Node 0 ├─15─┤ Node 1 |
+            # └───┬────┘    └───┬────┘
+            #     |     \  /    |
+            #     20     20     20
+            #     |     /  \    |
+            # ┌───┴────┐    ┌───┴────┐
+            # | Node 2 ├─15─┤ Node 3 |
+            # └────────┘    └────────┘
+            #
+            qemu-system-${QEMU} \
+                -kernel $KERNEL \
+                -initrd $INITRD \
+                -m 8192 \
+                -smp cpus=${NR_CPUS} \
+                -numa node,cpus=0-3,nodeid=0 \
+                -numa node,cpus=4-7,nodeid=1 \
+                -numa node,cpus=8-11,nodeid=2 \
+                -numa node,cpus=12-15,nodeid=3 \
+                -numa dist,src=0,dst=1,val=15 \
+                -numa dist,src=2,dst=3,val=15 \
+                -numa dist,src=0,dst=2,val=20 \
+                -numa dist,src=0,dst=3,val=20 \
+                -numa dist,src=1,dst=2,val=20 \
+                -numa dist,src=1,dst=3,val=20 \
+                -cpu ${PROCESSOR%:*} \
+                -nographic \
+                -append "console=uart,io,0x3f8,9600 memblock=debug" \
+                "$@" 
         else
             qemu-system-${QEMU} \
                 -kernel $KERNEL \
