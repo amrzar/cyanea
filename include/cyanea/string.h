ac578c91ce543f542b83fc1b66b03a9cba1042f7
Amirreza Zarrabi <amrzar@gmail.com>
2024-07-23 17:24:08 +1000

    cyanea/include: do cleanup
    cleanup cyanea/include/string.h
    
    Signed-off-by: Amirreza Zarrabi <amrzar@gmail.com>

--- include/cyanea/string.h
+++ include/cyanea/string.h
@@ -5,60 +5,22 @@
 
 #include <cyanea/stddef.h>
 
-#ifndef __HAVE_ARCH_STRCMP
 int strcmp(const char *, const char *);
-#endif
-
-#ifndef __HAVE_ARCH_STRNCMP
 int strncmp(const char *, const char *, size_t);
-#endif
 
-#ifndef __HAVE_ARCH_STRLEN
 size_t strlen(const char *);
-#endif
-
-#ifndef __HAVE_ARCH_STRNLEN
 size_t strnlen(const char *, size_t);
-#endif
 
-#ifndef __HAVE_ARCH_STRSTR
 char *strstr(const char *, const char *);
-#endif
-
-#ifndef __HAVE_ARCH_STRNSTR
 char *strnstr(const char *, const char *, size_t);
-#endif
-
-#ifndef __HAVE_ARCH_STRCHR
 char *strchr(const char *, int);
-#endif
-
-#ifndef __HAVE_ARCH_STRCPY
 char *strcpy(char *, const char *);
-#endif
-
-#ifndef __HAVE_ARCH_STRCAT
 char *strcat(char *, const char *);
-#endif
-
-#ifndef __HAVE_ARCH_STRNCPY
 char *strncpy(char *, const char *, size_t);
-#endif
 
-#ifndef __HAVE_ARCH_MEMCPY
 void *memcpy(void *, const void *, size_t);
-#endif
-
-#ifndef __HAVE_ARCH_MEMSET
 void *memset(void *, int, size_t);
-#endif
-
-#ifndef __HAVE_ARCH_MEMMOVE
 void *memmove(void *, const void *, size_t);
-#endif
-
-#ifndef __HAVE_ARCH_MEMCMP
 int memcmp(const void *, const void *, size_t);
-#endif
 
 #endif /* __CYANEA_STRING_H__ */
