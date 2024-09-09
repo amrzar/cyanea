f5ba3021f9778508349ba82b64b9ff7c8a727638
Amirreza Zarrabi <amrzar@gmail.com>
2024-09-09 13:29:59 +1000

    params: cleanup and add comments
    Signed-off-by: Amirreza Zarrabi <amrzar@gmail.com>

--- include/cyanea/params.h
+++ include/cyanea/params.h
@@ -3,9 +3,6 @@
 #ifndef __CYANEA_PARAMS_H__
 #define __CYANEA_PARAMS_H__
 
-int __param_get_option(const char *, int, const char *, char *, int,
-    char (*)(const char *, int));
-
 int param_get_option(const char *cmdline, const char *option, char *buf, int buf_size);
 
 #endif /* __CYANEA_PARAMS_H__ */
--- ukernel/params.c
+++ ukernel/params.c
@@ -5,35 +5,41 @@
 #include <asm/setup.h>
 #include <cyanea/stddef.h>
 
-static char default_param_read_char(const char *cmdline, int n)
-{
-    return cmdline[n];
-}
-
-int __param_get_option(const char *cmdline, int size,
-    const char *option, char *buf, int buf_size,
-    char (*read_char)(const char *, int))
+/**
+ * __param_get_option() - Process command-line options.
+ * @cmdline: command-line.
+ * @cmdline_len: maximum length of command-line.
+ * @option: option to search in the command-line.
+ * @buf: result.
+ * @buf_len: length of result.
+ *
+ * For @cmdline ''op1=v1 op2=v2 op1=v3 op3'', for @option = 'op2', @buf is 'v2',
+ * for @option = 'op1', @buf is 'v3' (second 'op1' override the first 'op1'),
+ * for @option = 'op3', it returns -2.
+ *
+ * Return: -2 if @option is an existing boolean, -1 if @option does not exist.
+ * Otherwise, size of option value returned in @ref_buf.
+ */
+static int __param_get_option(const char *cmdline, int cmdline_len, const char *option, char *buf,
+    int buf_len)
 {
     char ch;
     int i = 0, n = -1;
     const char *op = NULL;
     char *buffer = buf;
 
-    enum {
-        ST_WORDSTART,           /* Start of word after a whitespace. */
-        ST_WORDCMP,             /* Search for the option -- compare word. */
-        ST_WORDSKIP,            /* Skip the word. */
-        ST_BUFCOPY,             /* Copying option's value to buffer */
-    } state = ST_WORDSTART;
-
     if (cmdline == NULL)
         return -1;
 
-    if (read_char == NULL)
-        read_char = default_param_read_char;
+    enum {
+        ST_WORDSTART,   /* Start of word after a whitespace. */
+        ST_WORDCMP,     /* Search for the option -- compare word. */
+        ST_WORDSKIP,    /* Skip the word. */
+        ST_BUFCOPY,     /* Copying option's value to buffer */
+    } state = ST_WORDSTART;
 
     do {
-        ch = read_char(cmdline, i);
+        ch = cmdline[i];
 
         switch (state) {
         case ST_WORDSTART:
@@ -66,32 +72,27 @@ int __param_get_option(const char *cmdline, int size,
                 state = ST_WORDSTART;
 
             break;
-
         case ST_BUFCOPY:
             if (ch == ' ')
-                /* Found, but continue till the end of 'cmdline' in case it is overridden. */
                 state = ST_WORDSTART;
-            else {
-                if (++n < buf_size)
-                    *buffer++ = ch;
-            }
+            else if (++n < buf_len)
+                *buffer++ = ch;
 
             break;
         }
-    } while ((ch) && (++i < size));
+    } while (ch && (++i < cmdline_len));
 
-    if (buf_size != 0)
+    if (buf_len != 0)
         *buffer = '\0';
 
     return n;
 }
 
-int param_get_option(const char *cmdline, const char *option,
-    char *buf, int buf_size)
+int param_get_option(const char *cmdline, const char *option, char *buf, int buf_len)
 {
-    int err = __param_get_option(cmdline, COMMAND_LINE_SIZE,
-            option, buf, buf_size, NULL);
+    int err;
 
+    err = __param_get_option(cmdline, COMMAND_LINE_SIZE, option, buf, buf_len);
     switch (err) {
     case -2:    /* ''BOOLEAN'' parameter. */
         return 0;
