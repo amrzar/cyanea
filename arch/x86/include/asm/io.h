eca313beddd56f2aae73aeba775707ed9796fa5f
Amirreza Zarrabi <amrzar@gmail.com>
2024-10-12 15:09:08 +1100

    style: run 'make style'
    Signed-off-by: Amirreza Zarrabi <amrzar@gmail.com>

--- arch/x86/include/asm/io.h
+++ arch/x86/include/asm/io.h
@@ -11,38 +11,38 @@ static inline void slow_down_io(void)
 }
 
 #define BUILDIO(bwl, bw, type) \
-static __always_inline void out ## bwl (type value, u16 port) \
-{ \
-    asm volatile("out" #bwl " %" #bw "0, %w1"   \
-        : : "a"(value), "Nd"(port));            \
-} \
-  \
-static __always_inline type in ## bwl (u16 port)  \
-{ \
-    type value;                                 \
-    asm volatile("in" #bwl " %w1, %" #bw "0"    \
-        : "=a"(value) : "Nd"(port));            \
-    return value;                               \
-}
+    static __always_inline void out ## bwl (type value, u16 port) \
+    { \
+        asm volatile("out" #bwl " %" #bw "0, %w1"   \
+            : : "a"(value), "Nd"(port));            \
+    } \
+    \
+    static __always_inline type in ## bwl (u16 port)  \
+    { \
+        type value;                                 \
+        asm volatile("in" #bwl " %w1, %" #bw "0"    \
+            : "=a"(value) : "Nd"(port));            \
+        return value;                               \
+    }
 
 BUILDIO(b, b, u8);              /* ''inb'', ''outb''. */
 BUILDIO(w, w, u16);             /* ''inw'', ''outw''. */
-BUILDIO(l,  , u32);             /* ''inl'', ''outl''. */
+BUILDIO(l,, u32);               /* ''inl'', ''outl''. */
 #  undef BUILDIO
 
 #define BUILDIO(bwl, type) \
-static inline void out ## bwl ## _p (type value, u16 port) \
-{ \
-    out ## bwl(value, port);                    \
-    slow_down_io();                             \
-} \
-  \
-static inline type in ## bwl ## _p (u16 port)   \
-{ \
-    type value = in ## bwl(port);               \
-    slow_down_io();                             \
-    return value;                               \
-}
+    static inline void out ## bwl ## _p (type value, u16 port) \
+    { \
+        out ## bwl(value, port);                    \
+        slow_down_io();                             \
+    } \
+    \
+    static inline type in ## bwl ## _p (u16 port)   \
+    { \
+        type value = in ## bwl(port);               \
+        slow_down_io();                             \
+        return value;                               \
+    }
 
 BUILDIO(b, u8);                 /* ''inb_p'', ''outb_p''. */
 BUILDIO(w, u16);                /* ''inw_p'', ''outw_p''. */
